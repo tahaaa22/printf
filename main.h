9f16a4520196fe7ebcb27dedd66d7a7e400976f3
Basem1166 <basem_elgalfy@yahoo.com>
2023-07-25 13:22:30 -0700

    idek anymore

--- main.h
+++ main.h
@@ -3,12 +3,13 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int print_int(int num);
 int _putchar(char c);
-int print_char(va_list args);
+int print_char(va_list list);
 int print_percentage(void);
-int print_string(va_list args);
+int print_string(va_list list);
 int _printf(const char *format, ...);
 
 #endif
